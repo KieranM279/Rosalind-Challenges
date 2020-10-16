ed9b016814202e172425b96794cfc61b5e788f7f
KieranM279 <70451332+KieranM279@users.noreply.github.com>
2020-10-16 17:53:09 +0100

    Protein Mass Calculator
    This is a small function (AAcounter()) which can calculate the mass of any protein sequence in string format. This was my solution to the Rosalind Challenge "Calculating Protein Mass".
    
    It creates a dictionary of each amino acid's mass and frequency within the protein sequence. It then uses that data to return the mass of the protein.

---
+++ ProteinMassCalculator.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 15:41:31 2020
+
+@author: kiera
+"""
+
+import os
+os.chdir("C:/Users/kiera/Desktop/Rosalind/ProteinMassCalc")
+
+sequence = 'AWALCCVFLRYPKACQLRNHSTSKCWRLPPAFNLDITADMENFLFAYNGKKWAREPGKCPRNACFFCLGEDALFPCYAQKVPCKWLMPESFHWAHVDTGNYQMAYQRTSWMPTSNVMIVSCLHCGLAQCNSECWEHVHRRLVLCTIFALKMVFSRGERIADSTEFTVCTYWNPAFNMFNFLNRYGAYIEALPFWMRMRMPLQWWNDTRYRDVMLHEHHYGLLLDPKAIRNKHTKVSMAPLADESAMTGQRWPYKAKYDAVAPDSSWQRVGWEHIVHLFEMIKSLSFNMPGYLWCKRANLRSIKTEGRYYFHLESAMAETAWSLYSLKYSDPVHEVRIEWPGCKAGPGYQSRCTGPHHQFHHPHPGHNSCTDCKDYQCASPHQICCNYWIAIWRYIGCHWVLKWMCEAGQFFEFIVVEWMWAHAQKQVCCRCECQGEDSDGLIDCTMHVTPLVRDTCPVFMSSMIMIVTMELYPTSDHDHMPWIIMWMTYFLHFSNSGMYANMVPHWTQSNTHTTKYQWIENTTFCLIQQWAAEDVEINPFMKHGYAEVGCLKEDINAKACRMPHGYAVPELRSFWWDTYKWVFASNVCFFTARVCRLDNQSMMHCNETRKLPWFPPVPMAPVGKAYSWMPSMNNQCITAYEYCDQEYDLMCEEVYHAMMPIVSTQRWMKWCNTFAILKEPWKQWQPCYYFYVFTTKYQRMPDKPRFDLEFEWWCCHLNNRGSDRETEVHKWFMDAKNGSSNQYHRLCYACRVPGGVPELFFWASDHSHYVKQIYRCADLITPWKFKDPASYDHMDDFEDHNNYLADTGMMLGYTTGPEMEMMPICDWECMTEQRQHVSAKIKTTKNYFQCERTIWWVNMICSVESSI'
+
+def AAcounter(sequence):
+    
+    # Initiate the dicionary and final mass
+    MassDict = {}
+    TotalMass = 0
+    
+    # Opens a text file which contains individual AA mass data
+    masses = open('mass.txt')
+    
+    # Creates a dictionary of dictionaries of the data in the mass data
+    for ln in masses:
+        ln = ln.strip()
+        ln = ln.split()
+        
+        temp = {'freq':0,'mass':ln[1]}
+        MassDict[ln[0]] = temp
+    
+    # Loops through the Dictionary and adds the frequencies of each amino acid in the sequence
+    for aa in MassDict:
+        
+        weightedMass = 0
+        
+        MassDict[aa]['freq'] = sequence.count(aa)
+        
+        # Calculates the weighted mass of the amino acids in the sequence
+        frequency = int(MassDict[aa]['freq'])
+        daltons = float(MassDict[aa]['mass'])
+        weightedMass = float(frequency*daltons)
+        
+        # Creates a cumulative mass of the protein sequence
+        TotalMass += weightedMass
+    
+    return(TotalMass)
+    
+ProteinMass = AAcounter(sequence)
+
+    
+    
