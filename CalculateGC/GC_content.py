33fd19184f9d31d4f2341a44a56925795bd488e0
KieranM279 <70451332+KieranM279@users.noreply.github.com>
2020-12-02 00:56:01 +0000

    Update Calc

--- CalculateGC/GC_content.py
+++ CalculateGC/GC_content.py
@@ -1,31 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 25 23:23:04 2020
 
-@author: kiera
-"""
-import os
 from Bio import SeqIO
-
-os.chdir('C:/Users/kiera/Desktop/Rosalind/GCcontent')
 
 # parse over the fasta file
 for seq_record in SeqIO.parse(open('rosalind_gc.txt'),'fasta'):
-    
+
     # isolate the sequence and its respective ID
     identity = str(seq_record.id)
     sequence = str(seq_record.seq)
-    
+
     # create a dictionary to store the base counts
     bases = {'A':0, 'T':0, 'G':0, 'C':0}
     for b in bases:
         bases[b] = sequence.count(b)
-    
+
     # calculate GC content
     GC = bases['G'] + bases['C']
     AT = bases['A'] + bases['T']
     content = (GC /(AT + GC)) * 100
-    
+
     #print the results
     print(identity)
     print(content)
